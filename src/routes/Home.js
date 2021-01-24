61efc643b45bdc93cc484918dee03803487e0fc7
Namukk <skadnr625@gmail.com>
2021-01-24 16:59:34 +0900

    Change data as strings and thumbnail

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
   // const getNweets = async () => {
   //   const dbNweets = await dbService.collection("Ynweets").get();
   //   dbNweets.forEach((document) => {
@@ -44,6 +45,22 @@ const Home = ({ userObj }) => {
     } = event;
     setNweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+      console.log(finishedEvent);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -54,7 +71,14 @@ const Home = ({ userObj }) => {
           placeholder="Write what u want to say"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Ynweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear photo</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
